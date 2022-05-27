f4675895f4092692c164efa98b8bfc83ad60c943
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-05-27 12:51:31 -0700

    week2

--- week2/stylizedList/stylizedList.js
+++ week2/stylizedList/stylizedList.js
@@ -1,1 +1,18 @@
-var 
+fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+  .then(response => response.json())
+  .then(data => cars(data));
+
+
+  function cars (data) {
+      for (i = 0; i< data.length; i++){
+        //   console.log(data[i].nome)
+      let h1 = document.createElement("li")
+    //   let h2 = document.createElement("li")
+        //   document.write (data[i].nome)
+        h1.textContent = data[i].nome + ":" + " " + data[i].codigo
+        // h2.textContent = data[i].codigo
+
+        document.body.append(h1)
+      }
+  }
+  
