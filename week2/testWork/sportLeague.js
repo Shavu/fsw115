fe5721810f9b0ff0a274381f060f1f7a7b140ac2
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-05-28 22:41:15 -0700

    week2

--- week2/testWork/sportLeague.js
+++ week2/testWork/sportLeague.js
@@ -1,6 +1,14 @@
-fetch('https://api-football-standings.azharimm.site/leagues')
-  .then(response => response.json())
-  .then(data => document.write(data));
+// fetch('https://api-football-standings.azharimm.site/leagues')
+//   .then(response => response.json())
+//   .then(data => document.write(data));
+
+const afcNorthTeams = ["Steelers" , "Browns" , "Bengals", "Ravens"];
+
+function teams (afcNorthTeams) {
+  return afcNorthTeams[0];
+  
+}
+
   
 
   
