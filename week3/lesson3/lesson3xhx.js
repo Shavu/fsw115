77a8bc8ea0c29e193033adbbbfc3a98cc3b43f6b
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-06-02 09:28:03 -0700

    week3

--- week3/lesson3/lesson3xhx.js
+++ week3/lesson3/lesson3xhx.js
@@ -1,7 +1,29 @@
 const xhr = new XMLHttpRequest() 
-    xhr.onreadystatechange = function {
-        if(xhr.readyState == 4 && xhr.status == 200 )  
+//examples => //Method     //url                        //Async?
+    xhr.open("GET","https://swapi.dev/api/people" , true)
+    // xhr.send()
+    
+    xhr.onreadystatechange = function (){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            // console.log(xhr.reqsonseText)
+            let data = JSON.parse(xhr.responseText)
+            // console.log(data)
+            // showData(data)  // NOT A GOOD Pratice 
+            showData(data.results)
+        } else if (xhr.readyState == 4 && xhr.status !== 200){
+            console.log(xhr.responseText)
+        }
     }
 
+function showData(data){
+    console.log(data)
+    for( let i = 0; i < data.length; i++){
+        const charactor  = document.createElement("h1")
+        charactor.textContent = data[i].name
+        // charactor.textContent = data.name
+        document.body.appendChild(charactor)
+    }
+}
+ 
     
 
