4c425140f2f0cbed5962c9569428ca0a1af37b6f
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-06-03 13:09:18 -0700

    week3

--- week3/assignment1/pokemonList/pokemonList.js
+++ week3/assignment1/pokemonList/pokemonList.js
@@ -1,12 +1,24 @@
-const xhr =  new XMLHttpRequest()
-xhr.open("GET" ,"https://pokeapi.co/api/v2/pokemon", true)
-xhr.send()
+const xhr =  new XMLHttpRequest();
+xhr.open("GET" ,"https://pokeapi.co/api/v2/pokemon", true);
+xhr.send();
 
-xhr.opreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200){
-        console.log(xhr.responeText)
+xhr.onreadystatechange = function(){
+    if(this.readyState === 4 && this.status === 200){
         let data = JSON.parse(xhr.responseText)
-    } //  else if(xhr.readyState === 4 && xhr.status !== 200){
-    //     console.log(xhr.responeText)
-    // }
+        displayData(data.results);
+     } else if(this.readyState === 4 && this.status !== 200){
+        console.log(xhr.responeText)
+    }
 }
+
+function displayData(data) {
+     data.forEach(function(element) {
+        let newItem = document.createElement("h3");
+        newItem.textContent = element.name;
+        document.body.appendChild(newItem);
+     } )
+     }
+
+
+
+
