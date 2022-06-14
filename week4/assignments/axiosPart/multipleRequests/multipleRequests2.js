26fb2054668d3f2499abadb707b6d32cf2c0ef96
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-06-13 17:58:26 -0700

    week4

--- week4/assignments/axiosPart/multipleRequests/multipleRequests2.js
+++ week4/assignments/axiosPart/multipleRequests/multipleRequests2.js
@@ -1,29 +1,29 @@
 // const { default: axios } = require("axios");
 
-// document.getElementById("submit").addEventListener("click" , getData);
+document.getElementById("submit").addEventListener("click" , getData);
 
-// function getData(){
-//     console.log("clicked")
-//     axios.get("https://api.thecatapi.com/v1/images/search")
-//     .then(response => {
-//         console.log(response.data)
-//         for(let i = 0; i < response.data.length; i++){
-//             const array = response.data[i].title;
-//             const div = document.createElement("li")
-//             div.setAttribute = array;
-//             document.body.appendChild(div);
-//             div.addEventListener("click" , function(){
-//                 div.style.textDecoration = "line-throught"
-//             });
+function getData(){
+    console.log("clicked")
+    axios.get("https://api.thecatapi.com/v1/images/search")
+    .then(response => {
+        console.log(response.data)
+        for(let i = 0; i < response.data.length; i++){
+            const array = response.data[i].description;
+            const div = document.createElement("li")
+            div.textContent = array;
+            document.body.appendChild(div);
+            div.addEventListener("click" , function(){
+                div.style.textDecoration = "line-throught"
+            });
             
-//         };
-//     })
-//     .catch(error => console.log(error));
-// }
+        };
+    })
+    .catch(error => console.log(error));
+}
 
-// var mRequest =  document.createElement(div)
-// mRequest.textContent = ""
-// document.body.appendChild(mRequest)
+var mRequest =  document.createElement(div)
+mRequest.textContent = ""
+document.body.appendChild(mRequest)
 
 
 
